4a262e80abc878afe0eb0d6608a5f728c18ce5df
hcl <625217314@qq.com>
2019-02-28 19:38:52 +0800

    init component:Button

---
+++ src/components/button/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :disabled="disabled"
+    :class="[
+      'hcl-button',
+      { 'is-loading': loading },
+      btnType,
+      btnSize
+    ]">
+    <span></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: {
+      type: String,
+      default: 'medium'
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    btnType() {
+      return 'hcl-button--' + this.type;
+    },
+    btnSize() {
+      return 'hcl-button--' + this.size;
+    }
+  }
+}
+</script>
