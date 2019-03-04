81f6be725a92758207d57bc17464b6d98717d4e9
hcl <625217314@qq.com>
2019-03-04 19:24:11 +0800

    update global css

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-  html, body, #app {
-    height: 100%;
-  }
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
-</style>
