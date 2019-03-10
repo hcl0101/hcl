3fd30076b66b681010f007f9a9ec81fd44386a7d
hcl0101 <625217314@qq.com>
2019-03-10 11:10:46 +0800

    update api for component: Button

--- src/views/button/api.js
+++ src/views/button/api.js
@@ -4,10 +4,40 @@ export default [
     data: [
       {
         '参数': 'type',
-        '说明': '类型',
+        '说明': '按钮类型',
         '类型': 'String',
-        '可选值': 'default | primary | | |', 
+        '可选值': 'default | primary | success| danger | text', 
         '默认值': 'default'
+      }, {
+        '参数': 'size',
+        '说明': '按钮大小',
+        '类型': 'String',
+        '可选值': 'large | medium | small', 
+        '默认值': 'medium'
+      }, {
+        '参数': 'disabled',
+        '说明': '是否禁用',
+        '类型': 'Boolean',
+        '可选值': 'true | false', 
+        '默认值': 'false'
+      }, {
+        '参数': 'loading',
+        '说明': '是否处于加载状态',
+        '类型': 'Boolean',
+        '可选值': 'true | false', 
+        '默认值': 'false'
+      }, {
+        '参数': 'loading',
+        '说明': '是否处于加载状态',
+        '类型': 'Boolean',
+        '可选值': 'true | false', 
+        '默认值': 'false'
+      }, {
+        '参数': 'icon',
+        '说明': '带图标按钮',
+        '类型': 'String',
+        '可选值': `个人去阿里图标库下载， class="iconfont xxx"`, 
+        '默认值': '-'
       }
     ]
   },
@@ -16,7 +46,7 @@ export default [
     data: [
       {
         '名称': '-',
-        '说明': '按钮名称'
+        '说明': '按钮中的内容'
       }
     ]
   }
