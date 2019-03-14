fe0a79c97142ad490911ffba921d293ff47e2076
hcl <625217314@qq.com>
2019-03-14 15:53:27 +0800

    update description

--- src/directives/transfer-dom.js
+++ src/directives/transfer-dom.js
@@ -63,7 +63,7 @@ const directive = {
     }
   },
   unbind(el) {
-    if (el.dataset && el.dataset.transfer !== 'true') return false;
+    // if (el.dataset && el.dataset.transfer !== 'true') return false;
     el.className = el.className.replace('v-transfer-dom', '');
     const ref$1 = el.__transferDomData;
     if (!ref$1) return;
